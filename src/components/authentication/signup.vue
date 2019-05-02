69d6f2b8b4b97ca7f8131e080ff6d58fbb4a9d81
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-02 17:23:51 +0700

    sosial google

--- src/components/authentication/signup.vue
+++ src/components/authentication/signup.vue
@@ -1,87 +1,120 @@
 <template>
-    <div class="signup container">
-        <form @submit.prevent="signup" class="card-panel">
-            <h2 class="center teal-text">Signup</h2>
-            <div class="field">
-                <label for="email">Email:</label>
-                <input type="email" name="email" v-model="email">
-            </div>
-            <div class="field">
-                <label for="password">Password:</label>
-                <input type="password" name="password" v-model="password">
-            </div>
-            <div class="field">
-                <label for="alias">Alias:</label>
-                <input type="text" name="alias" v-model="alias">
-            </div>
-            <p class="red-text center" v-if="feedback">{{ feedback }}</p>
-            <div class="field center">
-                <button class="btn teal">Signup</button>
-            </div>
-        </form>
-    </div>
+  <div class="signup container">
+    <form @submit.prevent="signup" class="card-panel">
+      <h2 class="center teal-text">Signup</h2>
+      <div class="field">
+        <label for="email">Email:</label>
+        <input type="email" name="email" v-model="email">
+      </div>
+      <div class="field">
+        <label for="password">Password:</label>
+        <input type="password" name="password" v-model="password">
+      </div>
+      <div class="field">
+        <label for="alias">Alias:</label>
+        <input type="text" name="alias" v-model="alias">
+      </div>
+      <p class="red-text center" v-if="feedback">{{ feedback }}</p>
+      <div class="field center">
+        <button class="btn teal">Signup</button>
+      </div>
+    </form>
+    <p class="red-text center">
+      or Sign In with Google
+      <br>
+      <button @click="socialLogin" class="social-button">
+        <img alt="Google Logo" src="../../../public/img/google.jpg">
+      </button>
+    </p>
+  </div>
 </template>
 
 <script>
-import slugify from 'slugify'
-import db from '@/firebase/init'
-import firebase from 'firebase'
+import slugify from "slugify";
+import db from "@/firebase/init";
+import firebase from "firebase";
 export default {
-    name: 'Signup',
-    data(){
-        return {
-            email: null,
-            password: null,
-            alias: null,
-            feedback: null,
-            slug: null
-        }
+  name: "Signup",
+  data() {
+    return {
+      email: null,
+      password: null,
+      alias: null,
+      feedback: null,
+      slug: null
+    };
+  },
+  methods: {
+    signup() {
+      if (this.alias && this.email && this.password) {
+        this.slug = slugify(this.alias, {
+          replacement: "-",
+          remove: /[$*_+~.()'"!\-:@]/g,
+          lower: true
+        });
+        let ref = db.collection("dangtuanhuyhachi").doc(this.slug);
+        ref.get().then(doc => {
+          if (doc.exists) {
+            this.feedback = "This alias already exists";
+          } else {
+            firebase
+              .auth()
+              .createUserWithEmailAndPassword(this.email, this.password)
+              .then(cred => {
+                ref.set({
+                  alias: this.alias,
+                  user_id: cred.user.uid
+                });
+              })
+              .then(() => {
+                this.$router.push({ name: "home" });
+              })
+              .catch(err => {
+                this.feedback = err.message;
+              });
+            this.feddback = "Alias free to use";
+          }
+        });
+      } else {
+        this.feedback = "You must enter an all fields";
+      }
     },
-    methods: {
-        signup(){
-            if(this.alias && this.email && this.password){
-                this.slug = slugify(this.alias, {
-                    replacement: '-',
-                    remove: /[$*_+~.()'"!\-:@]/g,
-                    lower: true
-                })
-                let ref = db.collection('dangtuanhuyhachi').doc(this.slug)
-                ref.get().then(doc => {
-                    if(doc.exists){
-                        this.feedback = 'This alias already exists'
-                    } else {
-                        firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
-                        .then(cred => {
-                            ref.set({
-                                alias: this.alias,
-                                user_id: cred.user.uid
-                            })
-                        }).then(() => {
-                            this.$router.push({ name: 'home'})
-                        })
-                        .catch(err => {
-                            this.feedback = err.message
-                        })
-                        this.feddback = 'Alias free to use'
-                    }
-                })
-            } else {
-                this.feedback = "You must enter an all fields"
-            }
-        }
+     socialLogin() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then((result) => {
+          this.$router.replace('home');
+        }).catch((err) => {
+          alert('Oops. ' + err.message)
+        });
+      }
     }
-}
+};
 </script>
 
 <style>
-.signup{
-    max-width: 400px;
-    margin-top: 60px;
+.signup {
+  max-width: 400px;
+  margin-top: 60px;
 }
-.signup h2{
-    font-size: 2.4em;
+.signup h2 {
+  font-size: 2.4em;
 }
-.signup .field{
-    margin-bottom: 16px;
+.signup .field {
+  margin-bottom: 16px;
+}
+.social-button {
+  width: 75px;
+  background: white;
+  padding: 10px;
+  border-radius: 100%;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  outline: 0;
+  border: 0;
+}
+.social-button:active {
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+}
+.social-button img {
+  width: 100%;
 }
 </style>
